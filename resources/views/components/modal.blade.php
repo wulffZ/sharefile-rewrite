1401fab1404a0bb28ba6e28cb3d29834e0b155be
wulffZ <mattje74@gmail.com>
2022-04-27 18:08:23 +0200

    Updated modal to feature actual items with SVG icons that redirect to the upload page.

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,15 +1,13 @@
-<div id="modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full justify-center items-center">
+<div id="modal" tabindex="-1" aria-hidden="true" class="fixed left-0 hidden w-full overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
     <div class="relative w-full h-full max-w-2xl p-4 md:h-auto">
-
-            <div class="flex items-start justify-between p-4 border-b">
+        <div class="relative w-full h-full">
+            <div class="flex items-start justify-between p-4 border-b ">
                 <button type="button" class="text-gray-200" data-modal-toggle="modal">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <svg class="w-5 h-5 fill-gray-100 hover:fill-purple-600 transition duration-250 ease-in-out" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
             </div>
-
-            <div class="p-6 space-y-6 border-b text-gray-100">
-                {{ $slot }}
-            </div>
+            {{ $slot }}
+        </div>
     </div>
 </div>
 
